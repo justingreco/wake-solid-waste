81d9c039f294104918046133986a6d9dedbe2442
Justin Greco <justin.greco@raleighnc.gov>
2014-05-09 11:55:32 -0400

    uploading code

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var map, geojson;
+function findNearest(latlng) {
+  var distance = 0,
+    closest = null;
+  $(geojson.getLayers()).each(function(i, l) {
+    if (i === 0) {
+      distance = latlng.distanceTo(l.feature.geometry.coordinates);
+    }
+    if (latlng.distanceTo(l.feature.geometry.coordinates) < distance) {
+      distance = latlng.distanceTo(l.feature.geometry.coordinates);
+      closest = l;
+    }
+  });
+  map.setView(closest.feature.geometry.coordinates.reverse(), 16);
+  closest.openPopup();
+}
+function createMap() {
+  map = L.map('map').setView([35.81889, -78.64447], 11);
+  L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(map);
+  $.getJSON("facilities.geojson", function (data) {
+    geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('<strong>'+feature.properties.operator+'</strong><br/>'+feature.properties.type+'<br/>'+feature.properties.address);
+      }
+    }).addTo(map);
+  });
+   var locate = L.control.locate().addTo(map);
+   map.on("locationfound", function (loc) {
+     locate.stopLocate();
+     map.setView(loc.latlng, 17);
+     findNearest(loc.latlng);
+   });
+}
+$(document).ready(function () {
+  createMap();
+});
