5c5a277086cab0fff3359ee812e71ac863c9e176
Justin Greco <justin.greco@raleighnc.gov>
2014-05-21 16:36:53 -0400

    updates 4/21/2014

--- js/main.js
+++ js/main.js
@@ -1,9 +1,89 @@
-var map, geojson;
+var map, geojson, locationGroup;
+function addLocationToMap(latlng) {
+		var icon = L.icon({
+			iconUrl: 'img/location.png',
+			iconSize: [14,14]
+		});
+		locationGroup.clearLayers();
+		locationGroup.addLayer(L.marker(latlng, {icon:icon}));
+}
+
+function getMaterials () {
+  $.getJSON("materials.json", function (data) {
+    $(data).each(function (i, mat) {
+      $("select").append("<option>"+mat.Type+"</option>");
+    });
+  });
+}
+
+function mapProperty (pin) {
+	$.ajax({
+		url: 'https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/find',
+		dataType: 'jsonp',
+		data: {
+			f: 'json',
+			searchFields: "PIN_NUM",
+			returnGeometry: true,
+			searchText: pin,
+			layers: "0,1",
+			sr: 4326
+		},
+	})
+	.done(function(data) {
+		if (data.results.length > 0) {
+			//parcelGraphic.clearLayers();
+			var geom = data.results[0].geometry;
+			//agsGeometry = geom;
+			$(geom.rings).each(function (i, r) {
+				var ring = [];
+				$(r).each(function(j, p) {
+
+					ring.push({lon:p[0], lat:p[1]})
+
+				});
+				var poly = L.polygon(ring);//.addTo(parcelGraphic);
+				map.fitBounds(poly.getBounds());
+				geometry = poly;
+        findNearest(poly.getBounds().getCenter());
+        addLocationToMap(poly.getBounds().getCenter());
+			});
+			//checkSelectedValue(account);
+		}
+	});
+}
+function searchByAddress (address, type) {
+	$.ajax({
+		url: 'http://mapstest.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/exts/PropertySOE/RealEstateSearch',
+		dataType: 'jsonp',
+		data: {
+			f: 'json',
+			type: type,
+			values: '["'+address+'"]'
+		},
+	})
+	.done(function(data) {
+		account = data.Accounts[0];
+		mapProperty(account.pin);
+	});
+}
+function setAddressSearch () {
+  $('.typeahead').typeahead({
+			name: "address",
+			remote:{
+				url: "http://mapstest.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/exts/PropertySOE/AutoComplete?f=json&type=address&input=%QUERY",
+				filter: function (resp) {
+					return resp.Results;
+				}
+			}
+    }).on("typeahead:selected",function (obj, datum, dataset) {
+		searchByAddress(datum.value, dataset);
+	});;
+}
 function findNearest(latlng) {
   var distance = 0,
     closest = null;
   $(geojson.getLayers()).each(function(i, l) {
-    var coords = l.feature.geometry.coordinates.reverse();
+    var coords = L.latLng(l.feature.geometry.coordinates[1], l.feature.geometry.coordinates[0])//l.feature.geometry.coordinates.reverse();
     if (i === 0) {
       distance = latlng.distanceTo(coords);
     }
@@ -12,7 +92,12 @@ function findNearest(latlng) {
       closest = l;
     }
   });
-  map.setView(closest.feature.geometry.coordinates.reverse(), 16);
+  var toLocation = L.latLng(closest.feature.geometry.coordinates[1], closest.feature.geometry.coordinates[0]);
+  //var toLocation = closest.feature.geometry.coordinates.reverse();
+  //map.setView(closest.feature.geometry.coordinates.reverse(), 16);
+  map.fitBounds(L.polyline([latlng, toLocation]).getBounds());
+  var dirLink = "https://www.google.com/maps/dir/"+latlng.lat+","+latlng.lng+"/"+toLocation.lat+","+toLocation.lng;
+  closest.getPopup().setContent(closest.getPopup().getContent()+"<br/><a class='directions' href='"+dirLink+"' target='_blank'>Get Directions</a>");
   closest.openPopup();
 }
 function createMap() {
@@ -25,13 +110,17 @@ function createMap() {
       }
     }).addTo(map);
   });
+  locationGroup = L.featureGroup().addTo(map);
    var locate = L.control.locate().addTo(map);
    map.on("locationfound", function (loc) {
      locate.stopLocate();
      map.setView(loc.latlng, 17);
      findNearest(loc.latlng);
+     addLocationToMap(loc.latlng);
    });
 }
 $(document).ready(function () {
   createMap();
+  setAddressSearch();
+  getMaterials();
 });
